5d2c31c00fc03653c10dc6778de8902a802d07e4
Kacham Thanvi <thanvikacham@gmail.com>
2026-08-24 23:52:37 +0530

    Update mysql_db.py

--- database/mysql_db.py
+++ database/mysql_db.py
@@ -1,10 +1,13 @@
+import os
 import mysql.connector
 
 connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="*****",      # Put your MySQL password here
-    database="smart_todo"
+    host=os.environ["DB_HOST"],
+    port=int(os.environ["DB_PORT"]),
+    user=os.environ["DB_USER"],
+    password=os.environ["DB_PASSWORD"],
+    database=os.environ["DB_NAME"],
+    ssl_disabled=False
 )
 
 cursor = connection.cursor(dictionary=True)
